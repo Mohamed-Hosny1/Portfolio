abc5a145e96c4bc4d22477c9b806f7cd6a5a3988
Mohamed-Hosny1 <hosnu0169@gmail.com>
2025-09-04 07:19:44 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var typed = new Typed("#typed", {
+  strings: [
+    "Hi, there!",
+    "Welcome To My Page",
+    "I'm Mohamed Hosny",
+    "Frontend Developer",
+    "Building Web Experiences",
+    "experience More than year",
+  ],
+  typeSpeed: 90,
+  backSpeed: 50,
+  loop: true,
+});
